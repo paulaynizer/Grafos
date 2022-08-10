df905f2b67feee97266141990bea52338ab91433
paulaynizer <86982539+paulaynizer@users.noreply.github.com>
2022-08-10 09:27:59 -0300

    Update 1706EstruturaDeDados.cpp

--- T1/1706EstruturaDeDados.cpp
+++ T1/1706EstruturaDeDados.cpp
@@ -4,145 +4,22 @@
 #include <vector>
 #include <limits>
 #include <queue>
-#include<stdio.h>
-#include<stdlib.h>
+
+
 
 using namespace std;
 
 enum Cor {WHITE,GRAY,BLACK};
-int color[1001], d[1001], p[1001], f[1001], t,vertex, edge;
+int color[1001], d[1001], p[1001], f[1001],vertex, edge;
 char nota[1001];
 int NIL = numeric_limits<int>::min();
 int INF = numeric_limits<int>::max();
 bool possivel;
 
-//*************** FILA ****************
-//Nó da fila que aponta para o prox nó
-typedef struct fila_no{
-	struct fila_no * prox;
-	int dado;
-}fila_no;
-
-//Ponteiros para inicio e fim da fila
-typedef struct fila_t{
-	fila_no * frente;
-	fila_no * final;
-}fila_t;
-
-fila_t * fila_cria(){
-	/*Inicializafila  */
-	fila_t * nova_fila = (fila_t*)malloc(sizeof(fila_t)); 
-	
-	nova_fila -> frente = NULL;
-	nova_fila -> final = NULL;
-    return nova_fila;
-}
-
-void fila_put(fila_t * fila, int val){
-	
-	//Cria nó
-	fila_no * novo_no = (fila_no*)malloc(sizeof(fila_no));
-	novo_no -> dado = val;
-	novo_no -> prox = NULL;
-
-	
-	if (fila -> frente == NULL){
-		fila -> frente = fila -> final = novo_no;
-	}
-	
-	else{
-		/*O que era o último elemento, vai
-		apontar para o novo último elemento*/
-		fila -> final -> prox = novo_no;
-		
-		/*O novo elemento vai ser o final da fila*/
-		fila -> final = novo_no;
-	}	
-
-}
-
-int fila_pop(fila_t * fila){
-	
-	if (fila -> frente){
 
 
-		
-		int val = fila -> frente -> dado;
-	
-		fila_no * temp = fila -> frente;
-		
-						
-		fila -> frente = fila -> frente -> prox;
-		free(temp);
-		return val;
-	}
-	
-	return -1;
-}
-
-
-//*******************************
-
-//****** Grafo ********
-typedef struct node {
-  int vertice;
-  struct node* next;
-}_node;
-
-
-typedef struct Graph {
-  int numVertices;
-   node** adj;
-}_Graph;
-
-// Create a node
-node * createNode(int v) {
-	node * newNode = (node*)malloc(sizeof(node));
-  	newNode->vertice = v;
-  	newNode->next = NULL;
-  	return newNode;
-}
-
-// Create a graph
-Graph * createAGraph(int vertices) {
-  Graph * graph = (Graph*)malloc(sizeof(Graph));
-  graph->numVertices = vertices;
-  
-  graph->adj = (node**)malloc(vertices * sizeof(struct node*));
-
-  int i;
-  for (i = 0; i < vertices; i++)
-    graph->adj[i] = NULL;
-
-  return graph;
-}
-
-// Add edge
-void addEdge(struct Graph* graph, int s, int d) {
-  // Add edge from s to d
-  node * newNode = createNode(d);
-  newNode->next = graph->adj[s];
-  graph->adj[s] = newNode;
-
-  // Add edge from d to s
-  newNode = createNode(s);
-  newNode->next = graph->adj[d];
-  graph->adj[d] = newNode;
-}
-
-
-
-void varinhaMagica(Graph* graph,int v, int u)
-{
-	nota[v]='A';
-	nota[u]='A';
-	
-
-}
-
-bool BFS(Graph* graph, int s, int k){
-    node * u = graph->adj[0];
-	if (s== k){
+bool BFS(vector<int> adj[], int s, int k){
+    if (s== k){
                 return true;
     }
     for(int u=0; u<=vertex; u++) {
@@ -154,42 +31,65 @@ bool BFS(Graph* graph, int s, int k){
     color[s] = GRAY;
     d[s] = 0;
     p[s] = NIL;
-    
-	fila_t * Q = fila_cria();
-    fila_put(Q, s);
 
-    while( Q) {
-    	
-    	int atual = fila_pop(Q);
-    
-    	node* temp = graph->adj[atual];
-	
+    queue<int> Q; Q.push(s);
 
-        while(temp) {
-        	int adjVertex = temp->vertice;
-            if (adjVertex== k){
+    while( !Q.empty()) {
+        
+        int u = Q.front(); Q.pop();
+        
+        for(int v=0; v<adj[u].size(); v++) {
+            if (v== k){
                 //cout<<"entraaq"<<endl;
                 return true;
             }
-            if(color[adjVertex] == WHITE) {
-                if (adjVertex== k){
+            if(color[adj[u][v]] == WHITE) {
+                if (adj[u][v]== k){
                     //cout<<"entraaq"<<endl;
                     return true;
                 }
-                color[adjVertex] = GRAY;
-                //d[adjVertex] = d[atual]+v;
-                p[adjVertex] = atual;
-                p[atual] = adjVertex;
-                fila_put(Q, adjVertex);
+                color[adj[u][v]] = GRAY;
+                d[adj[u][v]] = d[u] + v;
+                p[adj[u][v]] = u;
+                p[u] = adj[u][v];
+                Q.push(adj[u][v]);
             }
         }
-        color[temp->vertice] = BLACK;
+        color[u] = BLACK;
     }
     return false;
 }
+bool existeCaminho(vector<int> adj[], char nota[]){
+    int hasB;
+    for(int u=1;u<=vertex;u++){
+        //para cada vertice do grafo
+                
+        if(nota[u]=='B'){
+            //se a nota for b
+        	if(hasB){
+        	    //segundo b encontrado
+        		if(BFS(adj,hasB,u)){
+        		   nota[hasB]='A';//varinha mágica
+	               nota[u]='A';//varinha mágica
+				}
+			    hasB=0; 
+		    }
+    	    else{
+    	        hasB=u;//primeiro b encontrado
+	        
+    	    }
+        }
+    }
+	for(int u=1;u<=vertex;u++){
+		if(nota[u]=='B'){
+			return false;
+		}
+	}
+	return true;
+}
 int main()
 {
-	vector<int> adj[4001];
+
     int n;//numero de vertices
     int m;//numero de arestas
  
@@ -198,9 +98,9 @@ int main()
     {   
     	
         bool existe=true;
-        Graph* graph = createAGraph(n);
+        
         vertex=n;
-
+        vector<int> adj[vertex+1];
         char notas[1001];
         int u, v;
         int hasB=0;
@@ -208,12 +108,13 @@ int main()
         for(int i=1;i<=vertex;i++){
             cin>>nota[i];
         }
+        //
         for(int i =1;i<=m;i++){
             cin>>u>>v;
-            addEdge(graph,u,v);
-           
+            adj[u].push_back(v);
+            adj[v].push_back(u);
         }
-      	//conta a quantidade de B
+      	//conta a quantidade de b
         for(int u=1;u<=vertex;u++){
         	if(nota[u]=='B'){
         	    cont++;
@@ -221,28 +122,10 @@ int main()
 		}
         //se for par 
         if(cont%2==0){
-            for(int u=1;u<=vertex;u++){//para cada vertice do grafo
-                
-        	    if(nota[u]=='B'){//se a nota for B
-        		    if(hasB){//segundo B encontrado
-        		    	if(BFS(graph,hasB,u)){
-        		    		varinhaMagica(graph,hasB,u);//faz troca
-						}	    
-						hasB=0; 
-					}
-    			    else{
-    				    hasB=u;//primeiro B encontrado
-			    	}
-				}
-		    }
-			for(int u=1;u<=vertex;u++){//se todas as notas são A
-				if(nota[u]=='B'){
-					existe=false;
-				}
-			}
-			if(existe==true){
-				cout<<"Y"<<endl;
-			}
+            //verifica se existe caminho entre os dois bs
+            if(existeCaminho(adj, nota)){
+                cout<<"Y"<<endl;
+            }
 			else{
 				cout<<"N"<<endl;
 			}
