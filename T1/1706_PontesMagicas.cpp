d7a7f598976ba0e760fcf41279f244767bcaaaf0
paulaynizer <86982539+paulaynizer@users.noreply.github.com>
2022-08-13 14:30:02 -0300

    Update 1706_PontesMagicas.cpp

--- T1/1706_PontesMagicas.cpp
+++ T1/1706_PontesMagicas.cpp
@@ -3,8 +3,6 @@
 #include <limits>
 #include <queue>
 
-
-
 using namespace std;
 
 enum Cor {WHITE,GRAY,BLACK};
@@ -17,8 +15,9 @@ int INF = -1;
 //bfs para verificar se existe caminho entre s e k
 bool BFS(vector<int> adj[], int s, int k){
     if (s== k){
-                return true;
+        return true;
     }
+    //inicializa os vetores color[], p[] e d[]
     for(int u=0; u<=vertex; u++) {
         color[u] = WHITE;
         d[u] = INF;
@@ -29,30 +28,35 @@ bool BFS(vector<int> adj[], int s, int k){
     d[s] = INF;
     p[s] = NIL;
 
+    //inicia fila de nós a serem processados com s
     queue<int> Q; Q.push(s);
 
+    //enquanto a fila não estiver vazia
     while( !Q.empty()) {
-        
+        //u recebe o primeiro elemento da fila
         int u = Q.front(); Q.pop();
-        
+        //para cada vértice da lista de adjascencia de u
         for(int v=0; v<adj[u].size(); v++) {
-            
+            //se ainda não foi descoberto
             if(color[adj[u][v]] == WHITE) {
+                //se encontrou o vertice k existe um caminho
                 if (adj[u][v]== k){
-                    //cout<<"entraaq"<<endl;
                     return true;
                 }
                 color[adj[u][v]] = GRAY;
                 d[adj[u][v]] = d[u] + v;
                 p[adj[u][v]] = u;
                 p[u] = adj[u][v];
-                Q.push(adj[u][v]);
+                Q.push(adj[u][v]);//adiciona o vertice v da lista de adjacencia de u na fila Q
             }
         }
+        //finaliza vertice
         color[u] = BLACK;
     }
     return false;
 }
+//faz as trocas da varinha mágica e 
+//retorna se existe caminho entre todos os Bs encontrados
 bool existeCaminho(vector<int> adj[], char nota[]){
     int hasB=0;
     for(int u=1;u<=vertex;u++){
@@ -62,6 +66,8 @@ bool existeCaminho(vector<int> adj[], char nota[]){
             //se a nota for b
         	if(hasB){
         	    //segundo b encontrado
+                //chama BFS()para verificar se existe um caminho
+                //entre os dois Bs encontrados
         		if(BFS(adj,hasB,u)){
         		   nota[hasB]='A';//varinha mágica
 	               nota[u]='A';//varinha mágica
@@ -74,11 +80,14 @@ bool existeCaminho(vector<int> adj[], char nota[]){
     	    }
         }
     }
+    //verifica se restou uma nota B depois das trocas
 	for(int u=1;u<=vertex;u++){
 		if(nota[u]=='B'){
+            //se encontrou uma nota B retorna false
 			return false;
 		}
 	}
+    //todas as notas são A, retorna true
 	return true;
 }
 int main()
@@ -87,21 +96,23 @@ int main()
     int n;//numero de vertices
     int m;//numero de arestas
  
-  
+    //le numero de vértices e arestas
 	while (cin>> n>>m)
     {   
     	
-        bool existe=true;
-        
         vertex=n;
-        vector<int> adj[vertex+1];
-        char notas[1001];
-        int u, v;
-        int cont=0;
+        vector<int> adj[vertex+1];//cria o vetor de adjacência
+        char notas[1001];//vetor das notas musicais
+        int u, v; //arco(u,v) do grafo
+        int cont=0; //contador
         
-        //le nota
+        //le nota e conta a quantidade de Bs
         for(int i=1;i<=vertex;i++){
             cin>>nota[i];
+            //conta as notas Bs encontradas
+            if(nota[i]=='B'){
+        	    cont++;
+			}
         }
         //le e adiciona aresta
         for(int i =1;i<=m;i++){
@@ -109,13 +120,8 @@ int main()
             adj[u].push_back(v);
             adj[v].push_back(u);//grafo não direcionado
         }
-      	//conta a quantidade de b
-        for(int u=1;u<=vertex;u++){
-        	if(nota[u]=='B'){
-        	    cont++;
-			}
-		}
-        //se for par 
+      	
+        //se a quantidade de Bs for par 
         if(cont%2==0){
             //verifica se existe caminho entre os dois bs
             if(existeCaminho(adj, nota)){
